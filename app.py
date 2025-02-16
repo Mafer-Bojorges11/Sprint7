0efca39a6f6d2aa4df5538516435ef6c8d50123d
Mafer-Bojorges11 <mariaferbo1198@hotmail.com>
2025-02-16 02:56:08 -0600

    Proyecto

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+import plotly_express as px
+
+car_data = pd.read_csv(r'C:\Users\DELL\Downloads\vehicles_us.csv')
+
+st.header('autos')
+hist_button = st.button('Construir histograma')
+
+if hist_button:
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    fig = px.histogram(car_data, x="odometer")
+    st.plotly_chart(fig, use_container_width=True)
